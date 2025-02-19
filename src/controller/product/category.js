361bfa4e2773b93627b4e26de0b2cf290de7c4ec
Shubham Dhole <shubhamdhole215@gmail.com>
2025-02-19 22:46:34 +0530

    implemented category route

---
+++ src/controller/product/category.js
@@ -0,0 +1,11 @@
+// Category Route: fetches products from all categories
+
+import Category from "../../model/category";
+
+export const getAllCategories = async (req, reply) => {
+  try {
+    const categories = await Category.find();
+  } catch (error) {
+    return reply.status(500).send({ message: "An error occurred", error });
+  }
+};
