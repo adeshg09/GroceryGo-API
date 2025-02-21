bbfa26e5973c4d30fe716c940c5ec1e8faf6be38
Shubham Dhole <shubhamdhole215@gmail.com>
2025-02-21 16:13:15 +0530

    fixed empty response typo

--- src/controller/product/category.js
+++ src/controller/product/category.js
@@ -3,6 +3,7 @@ import Category from "../../model/category.js";
 export const getAllCategories = async (req, reply) => {
   try {
     const categories = await Category.find();
+    return reply.send(categories);
   } catch (error) {
     return reply.status(500).send({ message: "An error occurred", error });
   }
