08e4e0bbe1375c8537cbf160b53ac2234cf96976
Shubham Dhole <shubhamdhole215@gmail.com>
2025-02-21 22:57:21 +0530

    admin dashboard configurations done using adminjs

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,42 @@
+import "dotenv/config.js";
+import fastifySession from "@fastify/session";
+import ConnectMongoDBSession from "connect-mongodb-session";
+import { Admin } from "../model/index.js";
+
 export const PORT = process.env.PORT || 3000;
 export const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD;
+
+const MongoDBStore = ConnectMongoDBSession(fastifySession);
+
+export const sessionStore = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "session",
+});
+
+sessionStore.on("error", (error) => {
+  console.error("Session store error:", error);
+});
+
+export const autheticate = async (email, password) => {
+  if (email && password) {
+    if (user.password === password) {
+      return Promise.resolve({ email: email, password: password });
+    } else {
+      return null;
+    }
+  }
+  // Uncomment this when created Admin manually.
+  //   if (email && password) {
+  //     const user = await Admin.findOne({ email });
+  //     if (!user) {
+  //       return null;
+  //     }
+  //     if (email == "shubham@gmail.com" && user.password === "12345678") {
+  //       return Promise.resolve({ email: email, password: password });
+  //     } else {
+  //       return null;
+  //     }
+  //   }
+
+  return null;
+};
